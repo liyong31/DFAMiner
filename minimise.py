ce41167813d9b98aff6cb982e87d351fb24e9b99
liyong31 <liyong460@gmail.com>
2023-11-29 03:57:32 +0800

    format file

--- minimise.py
+++ minimise.py
@@ -1,12 +1,12 @@
 # the standard way to import PySAT:
 from pysat.formula import CNF
 from pysat.solvers import Solver
-from pysat.solvers import Glucose3,Cadical103,Cadical153,Gluecard4,Glucose42,Lingeling,MapleChrono
 
 import itertools
 
-from dfa import DFA,dict2dfa
+from dfa import DFA, dict2dfa
 from dfa.draw import write_dot
+
 
 def read_input_fa(name):
     bufsize = 65536
@@ -17,7 +17,7 @@ def read_input_fa(name):
     dag = []
     acc = set()
     rej = set()
-    with open(name) as infile: 
+    with open(name) as infile:
         # open this file
         while True:
             lines = infile.readlines(bufsize)
@@ -28,30 +28,31 @@ def read_input_fa(name):
                 if num_line == 0:
                     num_states = int(line_brk[0])
                     num_colors = int(line_brk[1])
-                    print("#S = " + str(num_states))
-                    print("#C = " + str(num_colors))
+                    # print("#S = " + str(num_states))
+                    # print("#C = " + str(num_colors))
                     dag = [dict() for i in range(num_states)]
                     num_line += 1
                 elif num_line == 1:
                     init_state = int(line_brk[0])
-                    print("init = " + str(init_state))
+                    # print("init = " + str(init_state))
                     num_line += 1
                 elif line[0] == 'a':
                     state = int(line_brk[1])
-                    print("acc = " + str(state))
+                    # print("acc = " + str(state))
                     acc.add(state)
                 elif line[0] == 'r':
                     state = int(line_brk[1])
-                    print("rej = " + str(state))
+                    # print("rej = " + str(state))
                     rej.add(state)
                 else:
                     # now it is after three
                     src_state = int(line_brk[1])
                     letter = int(line_brk[2])
                     dst_state = int(line_brk[3])
-                    print("src = " + str(src_state) + " letter = " + str(letter) + " dst = " + str(dst_state))
+                    # print("src = " + str(src_state) + " letter = " +
+                        #   str(letter) + " dst = " + str(dst_state))
                     dag[src_state][letter] = dst_state
-    return (dag, init_state, acc, rej, num_colors)            
+    return (dag, init_state, acc, rej, num_colors)
 
 # now we have trees, it is time to create numbers
 # 0 <= i < j <= n-1
@@ -59,42 +60,45 @@ def read_input_fa(name):
 # 2. e_{a,i,j}: i goes to j over letter a
 # 3. t_{i,j}: there is a transition from i to j
 # 4. r_{a,i,j}: there is a transition from i to j over a and no smaller transition
+
+
 def create_variables(n, alphabet, graph):
-    
+
     num = 1
     # variables for DFA transitions
     prs = [(i, a, j) for i in range(n) for a in alphabet for j in range(n)]
     prs_m = prs
-    edges = {element : (index + num) for index, element in enumerate(prs)}
+    edges = {element: (index + num) for index, element in enumerate(prs)}
 
     # variables for final states
     num += len(edges)
-    sub_edges = { (i, -1, -1) : (num + i) for i in range(n)}
+    sub_edges = {(i, -1, -1): (num + i) for i in range(n)}
 
     edges.update(sub_edges)
-    
+
     print("edge vars:\n" + str(edges))
     # variables for tree node and state
     num = len(edges) + 1
     prs = [(p, q) for p in range(len(graph)) for q in range(n)]
-    nodes = { element : (index + num) for index, element in enumerate(prs)}
+    nodes = {element: (index + num) for index, element in enumerate(prs)}
     print("node vars:\n" + str(nodes))
-    
+
     # variables to encode BFS tree for DFA
     num += len(nodes)
     prs = [(p, q) for p in range(n) for q in range(n)]
-    parents = { element : (index + num) for index, element in enumerate(prs)}
+    parents = {element: (index + num) for index, element in enumerate(prs)}
     print("para vars:\n" + str(parents))
-    
+
     num += len(parents)
-    t_aux = { element : (index + num) for index, element in enumerate(prs)}
+    t_aux = {element: (index + num) for index, element in enumerate(prs)}
     print("t vars:\n" + str(t_aux))
-    
+
     num += len(t_aux)
-    m_aux = { element : (index + num) for index, element in enumerate(prs_m)}
+    m_aux = {element: (index + num) for index, element in enumerate(prs_m)}
     print("m vars:\n" + str(m_aux))
 
     return (nodes, edges, parents, t_aux, m_aux)
+
 
 def search_vertex(gr, init_state, w):
     # from init to the state
@@ -115,11 +119,12 @@ def search_vertex(gr, init_state, w):
         # print("after index: " + str(curr_index))
         curr_state = succ
 
+
 def create_dfa_cnf(nodes, edges, init_state, n, alphabet, graph, pos, negs):
-    
+
     clauses = []
-    #A. deterministic transition formulas
-    #A.1. not d_(p, a, q) or not d_(p, a, q')
+    # A. deterministic transition formulas
+    # A.1. not d_(p, a, q) or not d_(p, a, q')
     prs = [(p, a) for p in range(n) for a in alphabet]
     sub_clauses = []
     for p, a in prs:
@@ -127,150 +132,163 @@ def create_dfa_cnf(nodes, edges, init_state, n, alphabet, graph, pos, negs):
         one_succ = [edges[p, a, q] for q in range(n)]
         sub_clauses = [one_succ] + sub_clauses
     clauses = sub_clauses + clauses
-    
-    #A.2. one edge over a letter
+
+    # A.2. one edge over a letter
     diff_succs = [(q, qp) for q in range(n) for qp in range(n)]
     diff_succs = list(filter(lambda x: x[0] != x[1], diff_succs))
     print("diff_succ: " + str(diff_succs))
-    sub_clauses = [ [0-edges[p,a,q], 0-edges[p,a,qp]] for (p, a) in prs for (q, qp) in diff_succs]
+    sub_clauses = [[0-edges[p, a, q], 0-edges[p, a, qp]]
+                   for (p, a) in prs for (q, qp) in diff_succs]
     clauses = sub_clauses + clauses
-    
-    #B. consistent with samples
-    #B.1. first ensure that the initial state is empty word
+
+    # B. consistent with samples
+    # B.1. first ensure that the initial state is empty word
     clauses += [[nodes[init_state, 0]]]
-    clauses += [ [0-nodes[init_state, i]] for i in range(1, n)]
-    #B.2. setup final states
+    clauses += [[0-nodes[init_state, i]] for i in range(1, n)]
+    # B.2. setup final states
     # final_node = search_vertex(graph, init_state, pos[0])
     # print("final node: " + str(final_node))
-    sub_clauses = [[0-nodes[final_node, q], edges[q,-1,-1]] for q in range(n) for final_node in pos]
-    
+    sub_clauses = [[0-nodes[final_node, q], edges[q, -1, -1]]
+                   for q in range(n) for final_node in pos]
+
     # reject_node = search_vertex(graph, init_state, negs[0])
     # print("reject node: " + str(reject_node))
-    sub_clauses = [[0-nodes[reject_node, q], -edges[q,-1,-1]] for q in range(n) for reject_node in negs] + sub_clauses
+    sub_clauses = [[0-nodes[reject_node, q], -edges[q, -1, -1]]
+                   for q in range(n) for reject_node in negs] + sub_clauses
 
     clauses = sub_clauses + clauses
     print("add clauses for samples")
-    #B.3. consistent with samples
-    prs = [ (nr, dr, letter) for (nr, dr) in enumerate(graph) for letter in alphabet]
+    # B.3. consistent with samples
+    prs = [(nr, dr, letter) for (nr, dr) in enumerate(graph)
+           for letter in alphabet]
     prs = list(filter(lambda x: (x[2] in x[1]), prs))
-    #print (str(prs[0]))
+    # print (str(prs[0]))
     # check whether node has a child whose name is a
     # (nr, p) /\ edge(p, a, q) => (nr', q)
-    sub_clauses = [[0-nodes[nr, p], 0-edges[p, letter, q], nodes[dr[letter], q]] 
+    sub_clauses = [[0-nodes[nr, p], 0-edges[p, letter, q], nodes[dr[letter], q]]
                    for (nr, dr, letter) in prs for p in range(n) for q in range(n)]
-    # (nr, p) /\ (nr', q) => edge(p, a, q) 
-    #sub_clauses += [[0-nodes[nr, p], edges[p, letter, q], 0-nodes[dr[letter], q]] 
+    # (nr, p) /\ (nr', q) => edge(p, a, q)
+    # sub_clauses += [[0-nodes[nr, p], edges[p, letter, q], 0-nodes[dr[letter], q]]
     #               for (nr, dr, letter) in prs for p in range(n) for q in range(n)]
     clauses = sub_clauses + clauses
-    
+
     return clauses
 
 # Symmetry breaking by enforcing a BFS-tree on the generated
 # DFA, so the form is unique
 # reference to "BFS-Based Symmetry Breaking Predicates
 # for DFA Identification"
+
+
 def create_BFStree_cnf(edges, parents, t_aux, m_aux, n, alphabet):
-    
+
     clauses = []
-    #C. node BFS-tree constraints
-    #1. t_{i,j} <-> there is a transition from i to j
+    # C. node BFS-tree constraints
+    # 1. t_{i,j} <-> there is a transition from i to j
     prs = [(p, q) for p in range(n) for q in range(n)]
-    #1.1 e(p,a,q) => t(p,q)
-    sub_cluases = [ [0 - edges[p, a, q], t_aux[p, q]] for a in alphabet for (p, q) in prs]
-    #1.2 t(p, q) => some e(p, a, q)
+    # 1.1 e(p,a,q) => t(p,q)
+    sub_cluases = [[0 - edges[p, a, q], t_aux[p, q]]
+                   for a in alphabet for (p, q) in prs]
+    # 1.2 t(p, q) => some e(p, a, q)
     for p, q in prs:
         edge_rel = [edges[p, a, q] for a in alphabet]
-        edge_rel = [0- t_aux[p, q]] + edge_rel
+        edge_rel = [0 - t_aux[p, q]] + edge_rel
         sub_cluases = [edge_rel] + sub_cluases
-    
+
     clauses = sub_cluases + clauses
-    
-    #2. BFS tree order
-    #2.1 p_{j, i} i is the parent of j
+
+    # 2. BFS tree order
+    # 2.1 p_{j, i} i is the parent of j
     sub_cluases = []
     for j in range(1, n):
         # only one p_{j, 0}, p_{j,1} ... only one parent
-        
-        one_parent = [ parents[j, i] for i in range(j)]
-        sub_cluases = [one_parent] + sub_cluases 
-        
+
+        one_parent = [parents[j, i] for i in range(j)]
+        sub_cluases = [one_parent] + sub_cluases
+
         # p_{j,i} => t_{i,j}
-        exist_edges = [ [0- parents[j, i], t_aux[i, j]] for i in range(j)]
+        exist_edges = [[0 - parents[j, i], t_aux[i, j]] for i in range(j)]
         sub_cluases = exist_edges + sub_cluases
-        
+
         # t_{i,j} /\ !t_{i-1, j} /\ !t_{i-2,j} /\ ... /\ !t_{0,j} => p_{j,i}
         for i in range(j):
-            no_smaller_pred = [ t_aux[k, j] for k in range(i)]
-            no_smaller_pred = [0-t_aux[i, j], parents[j,i] ] + no_smaller_pred
+            no_smaller_pred = [t_aux[k, j] for k in range(i)]
+            no_smaller_pred = [0-t_aux[i, j], parents[j, i]] + no_smaller_pred
             sub_cluases = [no_smaller_pred] + sub_cluases
-            
+
     clauses = sub_cluases + clauses
-        
-    ij_pairs = [ (i, j) for j in range(n) for i in range(n)]
+
+    ij_pairs = [(i, j) for j in range(n) for i in range(n)]
     ij_pairs = list(filter(lambda x: x[0] < x[1], ij_pairs))
-    
+
     sub_cluases = []
     for i, j in ij_pairs:
         # only k < i < j, p_{j,i} => ! t_{k,j}
         # if i is parent of j in the BFS-tree, then no edge from k to j
-        # otherwise k will traverse j first 
+        # otherwise k will traverse j first
         k_vals = list(range(i))
-        no_smaller_edges = [ [0-parents[j,i], 0-t_aux[k, j]] for k in k_vals]
+        no_smaller_edges = [[0-parents[j, i], 0-t_aux[k, j]] for k in k_vals]
         sub_cluases = no_smaller_edges + sub_cluases
-    
+
     clauses = sub_cluases + clauses
-    
-    #3. relation to edges
+
+    # 3. relation to edges
     # m_{i, a, j} => e_{i, a, j}
-    edge_rel = [ [0-m_aux[i, a, j], edges[i, a, j]] for a in alphabet for (i, j) in ij_pairs]
-    
-    kh_pairs = [ (k, h) for h in alphabet for k in alphabet]
+    edge_rel = [[0-m_aux[i, a, j], edges[i, a, j]]
+                for a in alphabet for (i, j) in ij_pairs]
+
+    kh_pairs = [(k, h) for h in alphabet for k in alphabet]
     kh_pairs = list(filter(lambda x: x[0] < x[1], kh_pairs))
     # larger h > k, m_{i, h, j} => ! e_{i, k, j}
     # if there is a larger letter over i -> j in the BFS-tree,
     # then there must be no edge from i to j over a smaller letter
-    edge_rel = [ [0-m_aux[i, h, j], 0-edges[i, k, j]] 
+    edge_rel = [[0-m_aux[i, h, j], 0-edges[i, k, j]]
                 for (k, h) in kh_pairs for (i, j) in ij_pairs] + edge_rel
     clauses = edge_rel + clauses
-    
+
     # for every (i,j), e_{i,h,j} /\ ! e_{i,h-1,j} /\ .../\ !e_{i,0,j} => m_{i,h,j}
     # that is, if h is the smallest letter from i to j, then it is in BFS tree
     sub_cluases = []
-    prs = [ (i, j, a) for (i,j) in ij_pairs for a in alphabet]
+    prs = [(i, j, a) for (i, j) in ij_pairs for a in alphabet]
     for i, j, a in prs:
-        edge_rel = [edges[i, h, j] for h in range(a)] # smaller letters
+        edge_rel = [edges[i, h, j] for h in range(a)]  # smaller letters
         edge_rel = [0-edges[i, a, j], m_aux[i, a, j]] + edge_rel
         sub_cluases = [edge_rel] + sub_cluases
-    
+
     clauses = sub_cluases + clauses
-    
-    #4. BFS tree parent-child relation
-    ijk_pairs = [ (k, i, j) for k in range(n-1) for i in range(n-1) for j in range(n-1)]
+
+    # 4. BFS tree parent-child relation
+    ijk_pairs = [(k, i, j) for k in range(n-1)
+                 for i in range(n-1) for j in range(n-1)]
     ijk_pairs = list(filter(lambda x: x[0] < x[1] and x[1] < x[2], ijk_pairs))
     # p_{j, i} => !p_{j+1, k}, it means that i is parent of j, then k is not possible to be the parent of j + 1
-    # since k is even smaller than i 
-    edge_rel = [ [0- parents[j,i], 0-parents[j+1, k]] for (k, i, j) in ijk_pairs]
+    # since k is even smaller than i
+    edge_rel = [[0 - parents[j, i], 0-parents[j+1, k]]
+                for (k, i, j) in ijk_pairs]
 
-    ij_pairs = [ (i, j) for j in range(n-1) for i in range(n-1)]
+    ij_pairs = [(i, j) for j in range(n-1) for i in range(n-1)]
     ij_pairs = list(filter(lambda x: x[0] < x[1], ij_pairs))
     # (6)
     # p_{j,i} /\ p_{j+1, i} /\ m_{i,h,j} => !m_{i,k,j+1}
     # if i is parent of both j and j + 1, and in the BFS-tree, we have i ->j over h,
     # then there is no smaller letter k from i to j+1?
-    edge_rel = [[ 0-parents[j,i], 0-parents[j+1, i], 0-m_aux[i, h, j], 0-m_aux[i, k, j+1]]
+    edge_rel = [[0-parents[j, i], 0-parents[j+1, i], 0-m_aux[i, h, j], 0-m_aux[i, k, j+1]]
                 for (i, j) in ij_pairs for (k, h) in kh_pairs] + edge_rel
-    
+
     clauses = edge_rel + clauses
-    
+
     return clauses
-    
+
+
 def create_cnf(nodes, edges, parents, t_aux, m_aux, init_state, n, alphabet, graph, pos, negs):
-    clauses = create_dfa_cnf(nodes, edges, init_state, n, alphabet, graph, pos, negs)
+    clauses = create_dfa_cnf(nodes, edges, init_state,
+                             n, alphabet, graph, pos, negs)
     sub_clauses = create_BFStree_cnf(edges, parents, t_aux, m_aux, n, alphabet)
     clauses = sub_clauses + clauses
     for cls in clauses:
         print(cls)
     return clauses
+
 
 def construct_dfa_from_model(model, edges, n, alphabet):
     # print("type(model) = " + str(type(model)))
@@ -285,7 +303,7 @@ def construct_dfa_from_model(model, edges, n, alphabet):
         for a, letter in enumerate(alphabet):
             # print("a =" +str(a) + ", letter=" + str(letter))
             for q in range(n):
-                if model[edges[p, a, q] -1 ] > 0:
+                if model[edges[p, a, q] - 1] > 0:
                     # print("var = " + str(e1[p, a, q]))
                     # print("letter " + str(letter) + " -> " + str(q))
                     trans[letter] = q
@@ -294,50 +312,46 @@ def construct_dfa_from_model(model, edges, n, alphabet):
     dfa = dict2dfa(dfa_dict, start=0)
     # write_dot(dfa, "./dfa1.dot")
     return dfa
-    
+
+
 def solve(
-    sat
-    , n
-    , alphabet
-    , init_state
-    , graph
-    , pos
-    , negs):
-    
+        sat, n, alphabet, init_state, graph, pos, negs):
+
     nodes, edges, parents, t_aux, m_aux = create_variables(n, alphabet, graph)
     # solvers, Glucose3(), Cadical103(), Cadical153(), Gluecard4(), Glucose42()
-    #g = Cadical153() #Lingeling() #Glucose42()
-    #g = Glucose42()
+    # g = Cadical153() #Lingeling() #Glucose42()
+    # g = Glucose42()
     g = Solver(name=sat)
-    
-    clauses = create_cnf(nodes, edges, parents, t_aux, m_aux, init_state, n, alphabet, graph, pos, negs)
-    print("#Vars: " + str(len(nodes) + len(edges) + len(parents) + len(t_aux) + len(m_aux)))
+
+    clauses = create_cnf(nodes, edges, parents, t_aux, m_aux,
+                         init_state, n, alphabet, graph, pos, negs)
+    print("#Vars: " + str(len(nodes) + len(edges) +
+          len(parents) + len(t_aux) + len(m_aux)))
     print("#Clauses: " + str(len(clauses)))
     print("#N: " + str(n))
     print("#Pos: " + str(len(pos)))
     print("#Neg: " + str(len(negs)))
     for cls in clauses:
         g.add_clause(cls)
-    
+
     is_sat = g.solve()
     print(is_sat)
 
     if is_sat:
         print(g.get_model())
 
-        model= g.get_model()
+        model = g.get_model()
         # now print out transition relation
         # print("type(model) = " + str(type(model)))
         dfa = construct_dfa_from_model(model, edges, n, alphabet)
         return (True, dfa)
     else:
-        return (False, None)    
-                
-     
+        return (False, None)
+
 
 def main(infa, outfa, sat):
     dag, init_state, acc, rej, num_colors = read_input_fa(infa)
-    
+
     alphabet = list(range(num_colors))
     n = 1
     while True:
@@ -351,15 +365,15 @@ def main(infa, outfa, sat):
         else:
             n += 1
 
-solver_choices = {"cadical103", "cadical153"
-                  , "gluecard4", "glucose4", "glucose42"
-                  , "lingeling", "maplechrono"
-                  , "mergesat3", "minisat22"}
+
+solver_choices = {"cadical103", "cadical153", "gluecard4", "glucose4",
+                  "glucose42", "lingeling", "maplechrono", "mergesat3", "minisat22"}
 
 if __name__ == '__main__':
     import argparse
 
-    parser = argparse.ArgumentParser(description='Minimise automata with dont care words')
+    parser = argparse.ArgumentParser(
+        description='Minimise automata with dont care words')
     parser.add_argument('--infile', metavar='path', required=True,
                         help='path to input FA')
     parser.add_argument('--outfile', metavar='path', required=True,
